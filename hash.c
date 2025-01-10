c4b0ae8844c40194f8dd9d428961b85d819d42dd
Francesco Donnini <donnini.francesco00@gmail.com>
2025-01-10 14:15:11 +0100

    reworked hash() impl

--- hash.c
+++ hash.c
@@ -4,6 +4,31 @@
 #include <linux/printk.h>
 #include <linux/scatterlist.h>
 
+struct sdesc {
+    struct shash_desc shash;
+    char   *ctx;
+};
+
+static struct sdesc *init_sdesc(struct crypto_shash *alg) {
+    int size = sizeof(struct shash_desc) + crypto_shash_descsize(alg);
+    struct sdesc *sdesc = kmalloc(size, GFP_KERNEL);
+    if (!sdesc) {
+        return ERR_PTR(-ENOMEM);
+    }
+    sdesc->shash.tfm = alg;
+    return sdesc;
+}
+
+static int do_hash(struct crypto_shash *alg, const unsigned char *key, unsigned int len, unsigned char *digest) {
+    struct sdesc *sdesc = init_sdesc(alg);
+    if (IS_ERR(sdesc)) {
+        return PTR_ERR(sdesc);
+    }
+    int err = crypto_shash_digest(&sdesc->shash, key, len, digest);
+    kfree(sdesc);
+    return err;
+}
+
 /**
  * hash - calculates hash of key
  * @param alg_name the hashing algorithm to be used -e.g. sha1.
@@ -12,31 +37,12 @@
  * @param hash hash
  * @return 0 if the message digest creation was successfull, <0 otherwise (see crypto_shash_* API)
  */
-int hash(const char *alg_name, const char *key, int key_len, char *hash) {
-    int err;
-    struct shash_desc desc;
-    desc.tfm = crypto_alloc_shash(alg_name, 0, CRYPTO_ALG_TYPE_SHASH);
-    if (IS_ERR(desc.tfm)) {
-        printk(ss_pr_format("crypto_alloc_shash() failed."));
-        return PTR_ERR(desc.tfm);
+int hash(const char *alg_name, const char *key, int len, char *hash) {
+    struct crypto_shash *alg = crypto_alloc_shash(alg_name, CRYPTO_ALG_TYPE_SHASH, 0);
+    if (IS_ERR(alg)) {
+        return PTR_ERR(alg);
     }
-    err = crypto_shash_init(&desc);
-    if (err) {
-        printk(ss_pr_format("crypto_shash_init failed"));
-        goto free_shash;
-    }
-    err = crypto_shash_update(&desc, key, key_len);
-    if (err) {
-        printk(ss_pr_format("crypto_shash_update() failed"));
-        goto free_shash;
-    }
-    err = crypto_shash_final(&desc, hash);
-    if (err) {
-        printk(ss_pr_format("crypto_shash_final() failed"));
-        goto free_shash;
-    }
-
-free_shash:
-    crypto_free_shash(desc.tfm);
+    int err = do_hash(alg, key, len, hash);
+    crypto_free_shash(alg);
     return err;
 }
