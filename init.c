1332d5465ead6bc102fc53d08830f42d74804d15
Francesco Donnini <donnini.francesco00@gmail.com>
2025-01-10 17:24:28 +0100

    removed unused code from init

--- init.c
+++ init.c
@@ -10,14 +10,6 @@ MODULE_DESCRIPTION("Block-device snapshot");
 MODULE_LICENSE("GPL");
 
 static int __init snapshot_init(void) {
-    if (registry_init()) {
-        return 1;
-    }
-    registry_insert("sda1", "ciao");
-    if (registry_check_password("sda1", "ciao")) {
-        pr_debug(ss_pr_format("passwords don't match\n"));
-        return 0;
-    }
     return 0;
 }
 
