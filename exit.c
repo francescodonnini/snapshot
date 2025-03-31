2d2790fe7052c7eb0c19fa67da803d5b790622cd
Francesco Donnini <donnini.francesco00@gmail.com>
2025-03-31 17:34:19 +0200

    rimosso test

--- exit.c
+++ exit.c
@@ -4,8 +4,8 @@
 #include <linux/module.h>
 
 static void __exit snapshot_exit(void) {
-    // chrdev_cleanup();
-    // registry_cleanup();
+    chrdev_cleanup();
+    registry_cleanup();
 }
 
 module_exit(snapshot_exit);
--- init.c
+++ init.c
@@ -10,10 +10,9 @@
 #include <linux/types.h>
 
 static int __init snapshot_init(void) {
-    // chrdev_init();
-    // registry_init();
+    chrdev_init();
+    registry_init();
     init_procfs();
-    pr_debug(pr_format("found=%d\n"), find_mount("/home/francesco/Documents/singlefile-FS/mount"));
     return 0;
 }
 
