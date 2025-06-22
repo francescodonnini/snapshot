61e450af8b27ace54a10fa65f5e52605149b343f
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-23 00:55:25 +0200

    semplificato registrazione delle probe

--- probes.c
+++ probes.c
@@ -6,94 +6,54 @@
 #include <linux/kprobes.h>
 #include <linux/list.h>
 
-static struct kprobe kprobe_table[] = {
+static struct kprobe *kprobe_table[] = {
 };
-static size_t KPROBES_NUM = (sizeof(kprobe_table) / sizeof(struct kprobe));
+static size_t KPROBES_NUM = sizeof(kprobe_table) / sizeof(struct kprobe*);
 
-static int try_register_kprobe(struct kprobe *kp) {
-    int err = register_kprobe(kp);
+static struct kretprobe mount_bdev_kretp = {.kp={.symbol_name="mount_bdev"}, .entry_handler=mount_bdev_entry_handler, .handler=mount_bdev_handler};
+
+static struct kretprobe *kretprobe_table[] = {
+    &mount_bdev_kretp
+};
+static size_t KRETPROBES_NUM = sizeof(kretprobe_table) / sizeof(struct kretprobe*);
+
+static int register_all_kprobes(void) {
+    if (KPROBES_NUM <= 0) {
+        return 0;
+    }
+    int err = register_kprobes(kprobe_table, KPROBES_NUM);
     if (err) {
-        pr_debug(pr_format("cannot register kprobe for vfs_write, got error %d\n"), err);
+        pr_debug(pr_format("cannot register kprobes\n"));
     }
     return err;
 }
 
-static int register_kprobes(void) {
-    int i, err = 0;
-    for (i = 0; i < KPROBES_NUM && !err; ++i) {
-        err = try_register_kprobe(&kprobe_table[i]);
-    }
-    if (err) {
-        unregister(kprobe_table, i);
+static int register_all_kretprobes(void) {
+    if (KRETPROBES_NUM <= 0) {
         return 0;
     }
-    pr_debug(pr_format("all kprobes have been registered successfully!"));
-    return 0;
-}
-
-static struct kretprobe kretprobe_table[] = {
-    {.kp={.symbol_name="mount_bdev"}, .entry_handler=mount_bdev_entry_handler, .handler=mount_bdev_handler}
-};
-static size_t KRETPROBES_NUM = (sizeof(kretprobe_table) / sizeof(struct kretprobe));
-
-static int try_register_kretprobe(struct kretprobe *kp) {
-    int err = register_kprobe(kp);
+    int err = register_kretprobes(kretprobe_table, KRETPROBES_NUM);
     if (err) {
-        pr_debug(pr_format("cannot register kprobe for vfs_write, got error %d\n"), err);
+        pr_debug(pr_format("cannot register kretprobes\n"));
     }
     return err;
 }
-
-static int register_kretprobes(void) {
-    int i, err = 0;
-    for (i = 0; i < KPROBES_NUM && !err; ++i) {
-        err = try_register_kretprobe(&kprobe_table[i]);
-    }
-    if (err) {
-        unregister_kretprobe_until(kprobe_table, i);
-        return 0;
-    }
-    pr_debug(pr_format("all kprobes have been registered successfully!"));
-    return 0;
-}
-
-static void unregister_kprobe_until(struct kprobe *table, int n) {
-    while (--n >= 0) {
-        unregister_kprobe(&table[n]);
-    }
-}
-
-static void unregister_kretprobe_until(struct kretprobe *table, int n) {
-    while (--n >= 0) {
-        unregister_kretprobe(&table[n]);
-    }
-}
-
-static inline void unregister_kprobe_all(void) {
-    unregister_kprobe_until(kprobe_table, KPROBES_NUM);
-}
-
-static inline void unregister_kretprobe_all(void) {
-    unregister_kretprobe_until(kretprobe_table, KRETPROBES_NUM);
-}
-
-static void unregister_all(void) {
-    unregister_kprobe_all();
-    unregister_kretprobe_all();
-}
-
+ 
 int probes_init(void) {
-    int err = register_kprobes();
-    if (err) return 0;
-    err = register_kretprobes();
+    int err = register_all_kprobes();
     if (err) {
-        unregister_kprobe_all();
+        return err;
+    }
+    err = register_all_kretprobes();
+    if (err) {
+        return err;
     }
     return 0;
 
 }
 
 void probes_cleanup(void) {
-    unregister_all();
+    unregister_kprobes(kprobe_table, KPROBES_NUM);
+    unregister_kretprobes(kretprobe_table, KRETPROBES_NUM);
     pr_debug(pr_format("kprobes unregistered!"));
 }
