7460e0adb2f07faa9910e8e65752013eb0ea68b6
Francesco Donnini <donnini.francesco00@gmail.com>
2025-08-07 22:15:24 +0200

    rimossa stampa di debug in caso di successo nella registrazione del dispositivo

--- api/activate_snapshot.c
+++ api/activate_snapshot.c
@@ -10,6 +10,5 @@ int activate_snapshot(const char *dev_name, const char *password) {
         pr_debug(pr_format("cannot insert %s because of error %d\n"), dev_name, err);
         return err;
     }
-    pr_debug(pr_format("activate_snapshot(%s)\n"), dev_name);
     return 0;
 }
