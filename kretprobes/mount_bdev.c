41ae61467c43b498281e4fd25994f59ce4282a50
Francesco Donnini <donnini.francesco00@gmail.com>
2025-08-07 22:29:51 +0200

    removed some prints, add some comments

--- kretprobes/mount_bdev.c
+++ kretprobes/mount_bdev.c
@@ -20,10 +20,6 @@ static inline char* get_dev_name(struct pt_regs *regs) {
 }
 
 int mount_bdev_entry_handler(struct kretprobe_instance *kp, struct pt_regs *regs) {
-    pr_debug(
-        pr_format("mount_bdev(%s, %s) called\n"),
-        get_file_system_type(regs)->name,
-        get_dev_name(regs));
     // store the device name in the kretprobe instance data
     // so that it can be used in the return handler to check if the device has been successfully mounted
     struct mount_bdev_data *data = (struct mount_bdev_data*)kp->data;
@@ -40,6 +36,13 @@ static inline int try_update_loop_dev(struct block_device *bdev, char *buf) {
     }
 }
 
+/**
+ * registry_update_loop_device gets the path of the file backing the loop device and
+ * updates the registry with the device number associated to the image.
+ * Returns 0 on success, < 0 otherwise. Possible errors are -ENOMEM if it is not possible to make
+ * enough space to hold the path of the image file or if the update operation to the registry fails
+ * (see registry_update for more details).
+ */
 static int registry_update_loop_device(struct block_device *bdev) {
     char *buf = kmalloc(PATH_MAX, GFP_KERNEL);
     if (!buf) {
@@ -48,6 +51,11 @@ static int registry_update_loop_device(struct block_device *bdev) {
     int err = try_update_loop_dev(bdev, buf);
     kfree(buf);
     return err;
+}
+
+static inline void dbg_already_registered(const char *dev_name, struct block_device *bdev) {
+    pr_debug(pr_format("device %s associated to device number (%d, %d) is already registered"),
+             dev_name, MAJOR(bdev->bd_dev), MINOR(bdev->bd_dev));
 }
 
 /**
@@ -59,21 +67,15 @@ static int registry_update_loop_device(struct block_device *bdev) {
 int mount_bdev_handler(struct kretprobe_instance *kp, struct pt_regs *regs) {
     struct dentry *dentry = get_rval(struct dentry*, regs);
     if (IS_ERR(dentry)) {
-        pr_debug(pr_format("mount_bdev failed with error: %ld\n"), PTR_ERR(dentry));
+        pr_debug(pr_format("mount_bdev failed with error: %ld"), PTR_ERR(dentry));
         return 0;
     }
-
     struct mount_bdev_data *data = (struct mount_bdev_data*)kp->data;
     struct block_device *bdev = dentry->d_sb->s_bdev;
-    pr_debug(pr_format("mounted block device (%d, %d) with bdev %s\n"),
-             MAJOR(bdev->bd_dev), MINOR(bdev->bd_dev), data->dev_name);
-
     if (registry_lookup_mm(bdev->bd_dev)) {
-        pr_debug(pr_format("device %s associated to device number (%d, %d) is already registered"),
-                 data->dev_name, MAJOR(bdev->bd_dev), MINOR(bdev->bd_dev));
+        dbg_already_registered(data->dev_name, bdev);
         return 0;
     }
-
     if (is_loop_device(bdev)) {
         registry_update_loop_device(bdev);
     } else {
