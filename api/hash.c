55dfac8e94e392118b921061557eadcdedeea596
Francesco Donnini <donnini.francesco00@gmail.com>
2025-08-08 16:57:00 +0200

    le due funzioni di hash avevano porzioni di codice duplicato nonché una gestione sbagliata in caso di errore

--- api/hash.c
+++ api/hash.c
@@ -6,79 +6,85 @@
 #include <linux/string.h>
 #include <linux/types.h>
 
+static int init_shash(const char *alg_name, struct shash_desc **desc_out) {
+    int err = 0;
+    struct crypto_shash *alg = crypto_alloc_shash(alg_name, 0, 0);
+    if (IS_ERR(alg)) {
+        err = PTR_ERR(alg);
+        goto no_tfm_alloc;
+    }
+    struct shash_desc *desc;
+    size_t desc_size = crypto_shash_descsize(alg) + sizeof(*desc);
+    desc = kzalloc(desc_size, GFP_KERNEL);
+    if (desc == NULL) {
+        err = -ENOMEM;
+        goto no_desc_alloc;
+    }
+    desc->tfm = alg;
+    *desc_out = desc;
+    return 0;
+
+no_desc_alloc:
+    crypto_free_shash(alg);
+no_tfm_alloc:
+    return err;
+}
+
+static inline void free_shash_desc(struct shash_desc *desc) {
+    crypto_free_shash(desc->tfm);
+    kfree(desc);
+}
+
 /**
- * hash - calculates hash of key
+ * hash_alloc calculates hash of key using an algorithm specified by alg_name - e.g. sha1.
  * @param alg_name the hashing algorithm to be used -e.g. sha1.
  * @param key string to hash
  * @param key_len number of chars of key
  * @param hash hash
  * @return 0 if the message digest creation was successfull, <0 otherwise (see crypto_shash_* API)
  */
-char *hash(const char *alg_name, const char *key, int len) {
-    int err;
-    struct crypto_shash *alg = crypto_alloc_shash(alg_name, 0, 0);
-    if (IS_ERR(alg)) {
-        err = PTR_ERR(alg);
-        goto no_tfm_alloc;
-    }
+char *hash_alloc(const char *alg_name, const char *key, int len) {
     struct shash_desc *desc;
-    size_t desc_size = crypto_shash_descsize(alg) + sizeof(*desc);
-    desc = kzalloc(desc_size, GFP_KERNEL);
-    if (desc == NULL) {
-        err = -ENOMEM;
-        goto error;
+    int err = init_shash(alg_name, &desc);
+    if (err) {
+        return ERR_PTR(err);
     }
-    desc->tfm = alg;
-
-    size_t digest_size = crypto_shash_digestsize(alg);
-    char *out = kmalloc(digest_size, GFP_KERNEL);
+    char *out = kmalloc(crypto_shash_digestsize(desc->tfm), GFP_KERNEL);
     if (out == NULL) {
         err = -ENOMEM;
-        goto error;
+        goto no_out_buffer;
     }
     err = crypto_shash_digest(desc, key, len, out);
-    if (err < 0) {
-        goto no_hash_digest;
+    if (err) {
+        goto no_digest_out;
     }
-    crypto_free_shash(desc->tfm);
-    kfree(desc);
+    free_shash_desc(desc);
     return out;
 
-no_hash_digest:
+no_digest_out:
     kfree(out);
-error:
-    kfree(desc);
-    crypto_free_shash(desc->tfm);
-no_tfm_alloc:
+no_out_buffer:
+    free_shash_desc(desc);
     return ERR_PTR(err);
 }
 
-int hash2(const char *alg_name, const char *key, int key_len, char *out, int out_size) {
-    int err;
-    struct crypto_shash *alg = crypto_alloc_shash(alg_name, 0, 0);
-    if (IS_ERR(alg)) {
-        err = PTR_ERR(alg);
-        goto no_tfm_alloc;
-    }
+/**
+ * hash is the same as hash_alloc but it expects the caller to allocate a buffer to contains the hash of the key
+ */
+int hash(const char *alg_name, const char *key, int key_len, char *out, int out_size) {
     struct shash_desc *desc;
-    size_t desc_size = crypto_shash_descsize(alg) + sizeof(*desc);
-    desc = kzalloc(desc_size, GFP_KERNEL);
-    if (desc == NULL) {
-        err = -ENOMEM;
-        goto free_shash;
+    int err = init_shash(alg_name, &desc);
+    if (err) {
+        return err;
     }
-    desc->tfm = alg;
-    unsigned int digest_size = crypto_shash_digestsize(alg);
+    unsigned int digest_size = crypto_shash_digestsize(desc->tfm);
     if (digest_size > out_size) {
         pr_debug(pr_format("buffer reserved to contain hash digest is not big enough: expected %d but got %d\n"), digest_size, out_size);
         err = -1;
-        goto free_desc;
+        goto hash_out;
     }
     err = crypto_shash_digest(desc, key, key_len, out);
-free_desc:
-    kfree(desc);
-free_shash:
-    crypto_free_shash(desc->tfm);
-no_tfm_alloc:
+hash_out:
+    free_shash_desc(desc);
     return err;
 }
--- include/hash.h
+++ include/hash.h
@@ -1,8 +1,8 @@
 #ifndef AOS_HASH_H
 #define AOS_HASH_H
 
-char* hash(const char *alg_name, const char *key, int key_len);
+char* hash_alloc(const char *alg_name, const char *key, int key_len);
 
-int hash2(const char *alg_name, const char *key, int key_len, char *out, int out_size);
+int hash(const char *alg_name, const char *key, int key_len, char *out, int out_size);
 
 #endif
