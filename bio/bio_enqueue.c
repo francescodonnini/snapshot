4096b86d0f1a9281d085ac356ac6a5f141c377ba
Francesco Donnini <donnini.francesco00@gmail.com>
2025-08-20 19:31:30 +0200

    non avevo aggiornato il controllo sull'esito di bio_add_page, deve essere != dalla lunghezza indicata da bvec

--- bio/bio_enqueue.c
+++ bio/bio_enqueue.c
@@ -79,6 +79,7 @@ static void read_original_block_end_io(struct bio *bio) {
 }
 
 static inline int __add_page(struct bio *bio, int i, struct page *page, unsigned int len, unsigned int offset) {
+    pr_debug(pr_format("i=%d,len=%d,offset=%d"), i, len, offset);
     struct bio_private_data *p = bio->bi_private;
     if (i >= p->nr_pages) {
         pr_debug(pr_format("trying to add too much pages to bio block private data"));
@@ -91,13 +92,13 @@ static inline int __add_page(struct bio *bio, int i, struct page *page, unsigned
 }
 
 static inline int add_page(struct bio_vec *bvec, struct bio *bio, int i) {
-    pr_debug(pr_format("add_page(%d): [%d, %d)"), i, bvec->bv_offset, bvec->bv_offset);
+    pr_debug(pr_format("bvec=(len=%d,off=%d,page=%p)"), bvec->bv_len, bvec->bv_offset, bvec->bv_page);
     struct page *page = alloc_page(GFP_KERNEL);
     if (!page) {
         return -ENOMEM;
     }
     int err = bio_add_page(bio, page, bvec->bv_len, bvec->bv_offset);
-    if (err != PAGE_SIZE) {
+    if (err != bvec->bv_len) {
         __free_page(page);
         return -1;
     }
@@ -108,17 +109,22 @@ static int allocate_pages(struct bio *bio, struct bio *orig_bio) {
     int count = 0;
     int err = 0;
     struct bvec_iter old;
+    pr_debug(pr_format("before: (done=%d,idx=%d,sector=%llu,size=%d)"), orig_bio->bi_iter.bi_bvec_done, orig_bio->bi_iter.bi_idx, orig_bio->bi_iter.bi_sector, orig_bio->bi_iter.bi_size);
     memcpy(&old, &orig_bio->bi_iter, sizeof(struct bvec_iter));
     struct bio_vec bvec;
 	struct bvec_iter it;
     bio_for_each_bvec(bvec, orig_bio, it) {
+        pr_debug(pr_format("iter=(done=%d,idx=%d,sector=%llu,size=%d)"), it.bi_bvec_done, it.bi_idx, it.bi_sector, it.bi_size);
+        pr_debug(pr_format("bvec=(len=%d,off=%d,page=%p)"), bvec.bv_len, bvec.bv_offset, bvec.bv_page);
         int err = add_page(&bvec, bio, count);
         if (err) {
             goto allocate_pages_out;
         }
         ++count;
     }
+    pr_debug(pr_format("after: (done=%d,idx=%d,sector=%llu,size=%d)"), orig_bio->bi_iter.bi_bvec_done, orig_bio->bi_iter.bi_idx, orig_bio->bi_iter.bi_sector, orig_bio->bi_iter.bi_size);
     memcpy(&orig_bio->bi_iter, &old, sizeof(struct bvec_iter));
+    pr_debug(pr_format("restored: (done=%d,idx=%d,sector=%llu,size=%d)"), orig_bio->bi_iter.bi_bvec_done, orig_bio->bi_iter.bi_idx, orig_bio->bi_iter.bi_sector, orig_bio->bi_iter.bi_size);
     return err;
 
 allocate_pages_out:
@@ -129,34 +135,33 @@ allocate_pages_out:
     return err;
 }
 
-static struct bio_private_data *mk_private_data(struct bio *orig_bio) {
+/**
+ * create_read_bio creates a read request to the block IO layer. This request has a callback that schedules the original
+ * write bio after the targeted region has been read from the block device
+ */
+static struct bio* create_read_bio(struct bio *orig_bio) {
     struct bio_private_data *data;
     size_t size = sizeof(*data) + sizeof(struct page_iter) * orig_bio->bi_vcnt;
     data = kmalloc(size, GFP_KERNEL);
     if (!data) {
-        pr_debug(pr_format("cannot allocate enough memory for struct bio_private_data(%d)"), nr_pages);
+        pr_debug(pr_format("cannot allocate enough memory for struct bio_private_data(%d)"), orig_bio->bi_vcnt);
         return NULL;
     }
     data->orig_bio = orig_bio;
     data->nr_pages = orig_bio->bi_vcnt;
     data->sector = bio_sector(orig_bio);
-    return data;
-}
-
-static struct bio* create_read_bio(struct bio *orig_bio) {
-    struct bio_private_data *private_data = mk_private_data(orig_bio);
-    if (!private_data) {
+    if (!data) {
         return NULL;
     }
     struct bio *read_bio = bio_alloc(orig_bio->bi_bdev, orig_bio->bi_vcnt, REQ_OP_READ, GFP_KERNEL);
     if (!read_bio) {
         pr_debug(pr_format("bio_alloc failed"));
-        kfree(private_data);
+        kfree(data);
         return NULL;
     }
     read_bio->bi_iter.bi_sector = bio_sector(orig_bio);
     read_bio->bi_end_io = read_original_block_end_io;
-    read_bio->bi_private = private_data;
+    read_bio->bi_private = data;
     if (allocate_pages(read_bio, orig_bio)) {
         pr_debug(pr_format("allocate_pages failed"));
         kfree(read_bio->bi_private);
