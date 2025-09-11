a31d04964be838981e03809a602e679ab5207b94
Francesco Donnini <donnini.francesco00@gmail.com>
2025-09-11 10:46:18 +0200

    itree_subset_of() è stato aggiornato per rilevare se un intervallo è completamente coperto nell'albero degli intervalli

--- api/itree_rcu.c
+++ api/itree_rcu.c
@@ -20,15 +20,36 @@ int itree_add(struct session *s, struct b_range *range) {
     return mtree_store_range(&s->tree, range->start, range->end_excl, range, GFP_KERNEL);
 }
 
+static void update_span(struct b_range *q, struct b_range *r) {
+    if (q->start >= r->start && q->end_excl <= r->end_excl) {
+        // q:     [-----)
+        // r:  [------------)
+        q->start = q->end_excl;
+    } else if (q->start >= r->start) {
+        // if true then q->end_excl > r->end_excl
+        // q:       [---------------)
+        // r:  [------------)
+        q->start = r->end_excl;
+    } else if (q->end_excl <= r->end_excl) {
+        // if true then q->start < r->start
+        // q:  [----------)
+        // r:      [----------------)
+        q->end_excl = r->start;
+    }
+}
+
 /**
  * itree_subset_of returns true if the range identified by start and len is completely contained in the tree,
  * false otherwise.
  */
 bool itree_subset_of(struct session *s, unsigned long start, unsigned long end_excl) {
     rcu_read_lock();
-    long unsigned index = start;
-    struct b_range *r = mt_find(&s->tree, &index, end_excl);
-    bool contained = r && start >= r->start && end_excl <= r->end_excl;
+    struct b_range query = { .start = start, .end_excl = end_excl };
+    struct b_range *r;
+    unsigned long index = start;
+    mt_for_each(&s->tree, r, index, end_excl) {
+        update_span(&query, r);
+    }
     rcu_read_unlock();
-    return contained;
+    return query.start == query.end_excl;
 }
--- test/maple_tree/main.c
+++ test/maple_tree/main.c
@@ -4,26 +4,57 @@
 #include <linux/printk.h>
 
 struct b_range {
-    int a, b;
+    int lo, hi;
 };
 
 static struct b_range data[] = {
-    {.a=0, .b=100},
-    {.a=135, .b=200},
-    {.a=201, .b=250},
+    {.lo=0, .hi=150},
+    {.lo=850, .hi=1000},
+    {.lo=1000, .hi=1020},
+    {.lo=1020, .hi=1050},
+    {.lo=1051, .hi=1500},
+    {.lo=2000, .hi=2500},
 };
 static int n = sizeof(data) / sizeof(struct b_range);
 
 static int insert_all(struct maple_tree *t) {
     for (int i = 0; i < n; ++i) {
         struct b_range *r = &data[i];
-        int err = mtree_store_range(t, r->a, r->b, r, GFP_KERNEL);
+        int err = mtree_store_range(t, r->lo, r->hi, r, GFP_KERNEL);
         if (err) {
-            pr_err("cannot insert range [%d, %d), got error %d", r->a, r->b, err);
+            pr_err("cannot insert range [%d, %d), got error %d", r->lo, r->hi, err);
             return err;
         }
     }
     return 0;
+}
+
+static void update_span(struct b_range *q, struct b_range *r, unsigned long *span) {
+    if (q->lo >= r->lo && q->hi <= r->hi) {
+        q->lo = q->hi;
+    } else if (q->lo >= r->lo) {
+        q->lo = r->hi;
+    } else if (q->hi <= r->hi) {
+        q->hi = r->lo;
+    }
+    *span = q->hi - q->lo;
+}
+
+static bool mt_covers(struct maple_tree *tree, struct b_range *q) {
+    unsigned long span = 1;
+    struct b_range tmp;
+    memcpy(&tmp, q, sizeof(struct b_range));
+    struct b_range *r;
+    unsigned long index = q->lo;
+    mt_for_each(tree, r, index, q->hi) {
+        update_span(&tmp, r, &span);
+    }
+    return !span;
+}
+
+static void mt_test(struct maple_tree *tree, int test_no, struct b_range *query) {
+    char *result = mt_covers(tree, query) ? "yes" : "no";
+    pr_info("test %d: is query=[%d, %d) fully covered? %s", test_no, query->lo, query->hi, result);
 }
 
 static int __init mtree_test_init(void) {
@@ -32,26 +63,19 @@ static int __init mtree_test_init(void) {
     int err = insert_all(&tree);
     if (err) goto out;
 
-    unsigned long idx = 25;
-    unsigned long end = 125;
-    bool in = false;
-    struct b_range *r_prev = NULL;
-    struct b_range *r;
-    mt_for_each(&tree, r, idx, end) {
-        if (r_prev) {
-            if (r_prev->b < r->a) {
-                break;
-            }
-        }
-        pr_info("[%d, %d)", r->a, r->b);
-        if (end <= r->b) {
-            in = true;
-            break;
-        }
-        r_prev = r;
+    struct b_range queries[] = {
+        {.lo=25, .hi=125},
+        {.lo=75, .hi=175},
+        {.lo=900, .hi=1080},
+        {.lo=1010, .hi=1030},
+        {.lo=1900, .hi=2600},
+        {.lo=2700, .hi=3000},
+    };
+    for (int i = 0; i < sizeof(queries)/sizeof(struct b_range); ++i) {
+        mt_test(&tree, i+1, &queries[i]);
     }
+
 out:
-    pr_info("%d", in);
     mtree_destroy(&tree);
     return err;
 }
